05eff3b5e83dab967c2c3e4eb387d51785c837a1
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-05-27 18:17:13 -0300

    update voluntario

---
+++ src/main/java/br/com/impacto/voluntario/models/Voluntario.java
@@ -0,0 +1,37 @@
+package br.com.impacto.voluntario.models;
+
+import br.com.impacto.voluntario.enums.HabilidadesEnum;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "voluntarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class Voluntario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    @JsonIgnore
+    private String senha;
+    private Boolean ativo;
+    private LocalDate dataNascimento;
+    private LocalDate dataCadastro;
+    private Long necessidadesConcluidas;
+    private Endereco endereco;
+    private List<HabilidadesEnum> habilidades;
+    private String habilidadeOutro;
+}
